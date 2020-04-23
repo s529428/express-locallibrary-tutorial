b08f9109c68bf6fdd06a908d3243231a21d64991
Caroline Finnerty <s529428@nwmissouri.edu>
2020-04-23 15:25:01 -0500

    All of it yo

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,24 +1,76 @@
 var Genre = require('../models/genre');
+var Book = require('../models/book');
+var async = require('async');
 
 // Display list of all Genre.
-exports.genre_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre list');
+exports.genre_list = function(req, res, next) {
+    Genre.find()
+    .populate('genre')
+    .sort([['name','ascending']])
+    .exec(function (err, list_genres) {
+        if(err){return next(err);}
+        //Successful, so render
+        res.render('genre_list', {title: 'Genre List', genre_list: list_genres})
+    });
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_detail = function(req, res, next) {
+   
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+              .exec(callback);
+        },
+
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+              .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+    });
 };
 
 // Display Genre create form on GET.
-exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+exports.genre_create_get = function(req, res, next) {
+    res.render('genre_form', {title: 'Create Genre'});
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+    body('name', 'Genre name required'). isLength({min: 1}).trim(),
+    sanitizeBody('name').escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        var genre = new Genre({name: req.body.name});
+        if(!errors.isEmpty()){
+            res.render('genre_form', {title: 'Create Genre', genre: genre, errors: errors.array()});
+            return;
+        }else{
+            Genre.findOne({'name': req.body.name})
+            .exec(function(err, found_genre){
+                if (err) {return next(err);}
+                if(found_genre){
+                    res.redirect(found_genre.url);
+                }else{
+                    genre.save(function(err){
+                        if(err){return next(err);}
+                        res.redirect(genre.url);
+                    });
+                }
+            });
+        }
+    }
+]
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
